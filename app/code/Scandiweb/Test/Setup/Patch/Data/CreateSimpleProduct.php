d3d6aee14e0b64dbb2be1d14a50ebf5583bea850
Tamir Yirga <tamir.yirga@scandiweb.com>
2022-08-17 12:12:51 +0300

    Added inventory for  the product

--- app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
+++ app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
@@ -7,8 +7,11 @@ use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Type;
+use Magento\CatalogInventory\Model\Stock;
 use Magento\Framework\App\State;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 
 class CreateSimpleProduct implements DataPatchInterface
 {
@@ -33,23 +36,39 @@ class CreateSimpleProduct implements DataPatchInterface
     protected State $appState;
 
     /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
      * CreateSimpleProduct Constructor
      *
      * @param ProductInterfaceFactory $productFactory
      * @param ProductRepositoryInterface $productRepository
      * @param CategoryLinkManagementInterface $categoryLinkManagement
      * @param State $appState
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
      */
     public function __construct(
         ProductInterfaceFactory         $productFactory,
         ProductRepositoryInterface      $productRepository,
         CategoryLinkManagementInterface $categoryLinkManagement,
-        State                           $appState
+        State                           $appState,
+        SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface
     ) {
         $this->productFactory = $productFactory;
         $this->productRepository = $productRepository;
         $this->categoryLinkManagement = $categoryLinkManagement;
         $this->appState = $appState;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
     }
 
     /**
@@ -73,6 +92,15 @@ class CreateSimpleProduct implements DataPatchInterface
             ->setVisibility(4)
             ->setTypeId(Type::TYPE_SIMPLE);
         $product = $this->productRepository->save($product);
+
+        // Update the stock level
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setSku('SCANDIWEB-TEST-SKU');
+        $sourceItem->setQuantity(16);
+        $sourceItem->setStatus(Stock::STOCK_IN_STOCK);
+        $this->sourceItemsSaveInterface->execute([$sourceItem]);
+
         $categoryIds = [2]; // Default category id in store
         $this->categoryLinkManagement->assignProductToCategories($product->getSku(), $categoryIds);
     }
