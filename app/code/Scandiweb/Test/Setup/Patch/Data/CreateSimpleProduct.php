03fdf75b7abf17cd64759f3794a1c8f19f8a1a14
Tamir Yirga <tamir.yirga@scandiweb.com>
2022-08-17 12:03:41 +0300

    removed moduleDataSetup

--- app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
+++ app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
@@ -8,7 +8,6 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Type;
 use Magento\Framework\App\State;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
 class CreateSimpleProduct implements DataPatchInterface
@@ -22,11 +21,6 @@ class CreateSimpleProduct implements DataPatchInterface
      * @var ProductRepositoryInterface
      */
     protected ProductRepositoryInterface $productRepository;
-
-    /**
-     * @var ModuleDataSetupInterface
-     */
-    protected ModuleDataSetupInterface $moduleDataSetup;
 
     /**
      * @var CategoryLinkManagementInterface
@@ -43,20 +37,17 @@ class CreateSimpleProduct implements DataPatchInterface
      *
      * @param ProductInterfaceFactory $productFactory
      * @param ProductRepositoryInterface $productRepository
-     * @param ModuleDataSetupInterface $moduleDataSetup
      * @param CategoryLinkManagementInterface $categoryLinkManagement
      * @param State $appState
      */
     public function __construct(
         ProductInterfaceFactory         $productFactory,
         ProductRepositoryInterface      $productRepository,
-        ModuleDataSetupInterface        $moduleDataSetup,
         CategoryLinkManagementInterface $categoryLinkManagement,
         State                           $appState
     ) {
         $this->productFactory = $productFactory;
         $this->productRepository = $productRepository;
-        $this->moduleDataSetup = $moduleDataSetup;
         $this->categoryLinkManagement = $categoryLinkManagement;
         $this->appState = $appState;
     }
@@ -72,7 +63,6 @@ class CreateSimpleProduct implements DataPatchInterface
 
     public function createSimpleProduct()
     {
-        $this->moduleDataSetup->startSetup();
         $product = $this->productFactory->create();
         $product->setSku('SCANDIWEB-TEST-SKU')
             ->setName('Scandiweb Test Item')
@@ -85,7 +75,6 @@ class CreateSimpleProduct implements DataPatchInterface
         $product = $this->productRepository->save($product);
         $categoryIds = [2]; // Default category id in store
         $this->categoryLinkManagement->assignProductToCategories($product->getSku(), $categoryIds);
-        $this->moduleDataSetup->endSetup();
     }
 
     /**
